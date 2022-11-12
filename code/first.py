9275932f2cce1c3b4d5d249dab3d4a7f9c436983
Adela Cristina Patcas <adela.patcas@3pillarglobal.com>
2022-11-12 11:32:29 +0200

    setup + firstTest

---
+++ code/first.py
@@ -0,0 +1,24 @@
+import cv2
+import matplotlib.pyplot as plt
+
+fig = plt.figure(figsize=(7, 7))
+rows = 2
+columns = 1
+
+img="img/nature.jpg"
+
+InImgColor=cv2.imread(img)
+InImgGray=cv2.imread(img, 0)
+InImgRGB=cv2.cvtColor(InImgColor, cv2.COLOR_BGR2RGB)
+
+
+fig.add_subplot(rows, columns, 1)
+plt.imshow(InImgRGB)
+plt.axis('off')
+plt.title('The color input image')
+
+fig.add_subplot(rows, columns, 2)
+plt.imshow(InImgGray, cmap = 'gray')
+plt.axis('off')
+plt.title('The gray input image')
+plt.show()
